fbe09fc7ef02a2e6b8ee70ed9e8911868f0e86eb
silver-eunjoo <leeeun5423@naver.com>
2025-03-06 09:47:47 +0900

    5. 관계연산자

---
+++ basic2/src/main/java/lec2/RelationalOperator.java
@@ -0,0 +1,32 @@
+package lec2;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        // a Greater Than b
+        boolean aGtb = a > b;
+        System.out.println("aGtB = " + aGtb);
+
+        // a Less Than b
+        boolean aLtb = a < b;
+        System.out.println("aLtb = " + aLtb);
+
+        // a greater or equal than b
+        boolean aGeb = a >= b;
+        System.out.println("aGeb = " + aGeb);
+
+        // a little or equal than b
+        boolean aLeb = a <= b;
+        System.out.println("aLeB = " + aLeb);
+
+        // a equal b
+        boolean aEqb = a == b;
+        System.out.println("aEqb = " + aEqb);
+
+        // a not equal b
+        boolean aNeb = a != b;
+        System.out.println("aNeb = " + aNeb);
+
+    }
+}
