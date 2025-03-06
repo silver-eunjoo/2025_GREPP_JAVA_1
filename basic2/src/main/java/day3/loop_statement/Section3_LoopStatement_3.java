6f1462790cf098e049ada1d7fec78119614092ff
silver-eunjoo <>
2025-03-06 14:36:14 +0900

    6. 반복문 do-while

---
+++ basic2/src/main/java/day3/loop_statement/Section3_LoopStatement_3.java
@@ -0,0 +1,14 @@
+package day3.loop_statement;
+
+public class Section3_LoopStatement_3 {
+    public static void main(String[] args) { // refactor rename -> shift + fn6
+        int a =1;
+
+        do {
+            a *=2;
+        } while (a <10);
+
+        int b =1;
+        do b*=2; while(b<10);
+    }
+}
