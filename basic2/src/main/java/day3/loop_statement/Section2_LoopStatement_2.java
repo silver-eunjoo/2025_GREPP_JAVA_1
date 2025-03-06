0b4146f03b643e406e011465ed3f8607684d0cab
silver-eunjoo <>
2025-03-06 14:33:48 +0900

    6. 반복문 for

--- basic2/src/main/java/day3/loop_statement/Section2_LoopStatement_2.java
+++ basic2/src/main/java/day3/loop_statement/Section2_LoopStatement_2.java
@@ -1,28 +1,39 @@
 package day3.loop_statement;
 
 public class Section2_LoopStatement_2 {
-    public static void main(String[] args) {
-        /*
-        while(true) {
-            System.out.println("Hello World!");
-        }*/
-
-        int count = 0; // 초기식
-        while (count <= 10) { // 조건식
-            // 명령문
-            System.out.println("Hello World!");
-            count += 1;
+    public static void main(String[] args) { // refactor rename -> shift + fn6
+        //for
+        for (int i=0; i <= 10; i++) {
+            System.out.println("Hello world!");
         }
 
-        int b = 1;
-        while (b<10) b*=2;
-        System.out.println("Hello world!");
+        int a =0;
+        for(; a< 10; a++){
+            System.out.println("A");
+        }
 
-        int c = 1;
-        while (c <10)
-            c*=2;
-        System.out.println("Hello world!");
+        /*
+        for (int i=0;i<=10;i++){
+            for (int j=0;j<=10;j++){
+                for (int k=0;k<=10;k++){
 
+                }
+            }
+        }
+        */
 
+        for(int i=0,j=0;i<10;i++){
+            System.out.println("i = " + i);
+
+            if (i >4){
+                j++;
+            }
+
+            System.out.println("j = " + j);
+        }
+
+        for (int i=10;i>0;i--){
+            System.out.println("i = " + i);
+        }
     }
 }
