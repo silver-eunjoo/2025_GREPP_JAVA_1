3fee24c0f3c939835e1d80f436206d4a846ef4f7
silver-eunjoo <leeeun5423@naver.com>
2025-03-06 09:40:27 +0900

    4. 대입연산자

--- basic2/src/main/java/lec2/AssignOperator.java
+++ basic2/src/main/java/lec2/AssignOperator.java
@@ -1,4 +1,34 @@
 package lec2;
 
 public class AssignOperator {
+
+    public static void main(String[] args) {
+        int a;
+
+        a = 10;
+        System.out.println("a = " + a);
+
+        a = 20;
+        System.out.println("a = " + a);
+
+        a = (short)1; // 명시적 형변환
+        a = (byte) 1;
+
+        a = 10;
+
+        a += 1;
+        System.out.println("a = " + a);
+
+        a -= 1;
+        System.out.println("a = " + a);
+
+        a*=2;
+        System.out.println("a = " + a);
+
+        a/=2;
+        System.out.println("a = " + a);
+
+        a%=2;
+        System.out.println("a = " + a);
+    }
 }
