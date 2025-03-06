fcbbd593a56a0308408534a0c8eb805fe0034d98
silver-eunjoo <>
2025-03-06 11:04:52 +0900

    1: if문

--- basic2/src/main/java/day3/select_statement/Section1_if.java
+++ basic2/src/main/java/day3/select_statement/Section1_if.java
@@ -1,5 +1,31 @@
 package day3.select_statement;
 
 public class Section1_if {
-    
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("result : true");
+        }
+        if (false) {
+            System.out.println("result : false");
+        }
+
+        if (!true) {
+            System.out.println("result : not true");
+        }
+        if (!false) {
+            System.out.println("result : not false");
+        }
+
+        if (false) {
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(4);
+        }
+        System.out.println(5);
+
+//        if (true) System.out.println("result:true");
+        
+
+    }
 }
