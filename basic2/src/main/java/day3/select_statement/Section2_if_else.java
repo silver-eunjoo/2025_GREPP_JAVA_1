a9715db629621f706d1a357b4168a1fb36dc61a9
silver-eunjoo <>
2025-03-06 11:08:11 +0900

    2: if-else문

---
+++ basic2/src/main/java/day3/select_statement/Section2_if_else.java
@@ -0,0 +1,17 @@
+package day3.select_statement;
+
+public class Section2_if_else {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if(a>=10){
+            a = 0;
+        }else{
+            a = -1;
+        }
+
+        System.out.println("a = " + a);
+
+
+    }
+}
