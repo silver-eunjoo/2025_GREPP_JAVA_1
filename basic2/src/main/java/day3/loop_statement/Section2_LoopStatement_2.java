1d6fa51e96bd5e0a926ebcd766069d91f16eef2c
silver-eunjoo <leeeun5423@naver.com>
2025-03-06 14:24:46 +0900

    6. 반복문 while

---
+++ basic2/src/main/java/day3/loop_statement/Section2_LoopStatement_2.java
@@ -0,0 +1,28 @@
+package day3.loop_statement;
+
+public class Section2_LoopStatement_2 {
+    public static void main(String[] args) {
+        /*
+        while(true) {
+            System.out.println("Hello World!");
+        }*/
+
+        int count = 0; // 초기식
+        while (count <= 10) { // 조건식
+            // 명령문
+            System.out.println("Hello World!");
+            count += 1;
+        }
+
+        int b = 1;
+        while (b<10) b*=2;
+        System.out.println("Hello world!");
+
+        int c = 1;
+        while (c <10)
+            c*=2;
+        System.out.println("Hello world!");
+
+
+    }
+}
