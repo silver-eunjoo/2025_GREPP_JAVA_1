fe4d069a25f976cdfba4643bbe81022582bf3d76
silver-eunjoo <leeeun5423@naver.com>
2025-03-07 11:04:12 +0900

    1. 클래스와 추상화

---
+++ basic2/src/main/java/day4/class_and_abstraction/ClassName.java
@@ -0,0 +1,27 @@
+package day4.class_and_abstraction;
+
+/*
+I am apple.
+의미 단위로 이름지을 때, 두번째를 대문자로 쓰는 거 - Camel case
+
+i_am_apple - Snake case
+
+iAmApple - Camel case
+
+i-am-apple - Kebab case (css에서 씀)
+* */
+
+
+public class ClassName { //클래스 네임 첫글자는 무조건 대문자 + Camel case)
+    public int field1; // 상태
+    public int field2 = 2;
+
+    public void method1() { // 행동
+
+    }
+
+    // JPhone
+    // JPhone은 색상과 번호라는 상태를 갖는다.
+    // JPhone은 전화를 할 수 있다.
+
+}
