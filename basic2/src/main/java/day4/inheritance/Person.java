2468d024c1b139db32a4d3c2ac529290bb1d3ad5
silver-eunjoo <>
2025-03-10 11:27:49 +0900

    3. 상속

--- basic2/src/main/java/day4/inheritance/Person.java
+++ basic2/src/main/java/day4/inheritance/Person.java
@@ -1,5 +1,15 @@
 package day4.inheritance;
 
 public class Person {
-    
+    public String name;
+    public int age;
+    public int height;
+
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+
 }
