c033ae81b2e71459108477ce9b928180a5e4ff3f
silver-eunjoo <>
2025-03-10 11:16:19 +0900

    1. final keyword

--- basic2/src/main/java/day4/extends_example/final_keyword/s2/inner/FinalMethod.java
+++ basic2/src/main/java/day4/extends_example/final_keyword/s2/inner/FinalMethod.java
@@ -1,10 +1,24 @@
 package day4.extends_example.final_keyword.s2.inner;
 
-public interface FinalMethod {
+public class FinalMethod {
 
-    void method1();
-    void method2(int a, int b);
-    void method3(final int a, int b);
-    void method4(final int a, final int b);
+    public final void method1(){
+        System.out.println("Hello World!");
 
+    }
+
+    public void method2(int a, int b){
+        int sum = a + b;
+
+        System.out.println("sum = " + sum);
+
+        a = 20;
+        System.out.println("a = " + a);
+    }
+
+    public void method3(final int a, int b){
+        int sum = a + b;
+
+        System.out.println("sum = " + sum);
+    }
 }
